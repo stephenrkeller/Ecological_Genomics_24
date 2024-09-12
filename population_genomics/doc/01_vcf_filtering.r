3c3cde1286e219c4cd17b53f9394616106fcb4d9
stephenrkeller <srkeller@uvm.edu>
2024-09-12 14:44:16 -0400

    update script

--- population_genomics/doc/01_vcf_filtering.r
+++ population_genomics/doc/01_vcf_filtering.r
@@ -11,17 +11,28 @@ list.files("reference/")
 
 vcf <- read.vcfR("variants/Centaurea_filtered.vcf.gz")
 
-vcf2 <- read.vcfR("variants/Centaurea_nofilter.vcf.gz")
-
 dna <- ape::read.dna("reference/GCA_030169165.1_ASM3016916v1_genomic.fa.gz", format="fasta")
+#dna2 = dna$CM058040.1
 
 gff <- read.table("reference/GCA_030169165.1_ASM3016916v1_genomic_genes.gff.gz", sep="\t", quote="")
+#gff2 = grep("CM058040.1",gff)
+
+vcf
+
+head(vcf)
 
 # create the chromR object class
 chr1 <- create.chromR(name="Centaurea chr1", vcf=vcf, seq=dna, ann=gff, verbose=F)
+#doesn't work: chr2 <- create.chromR(name="Centaurea chr2", vcf=vcf, seq=dna2, ann=gff2, verbose=F)
+
+#head(chr1@var.info)
+#chr1
+#quantile(chr1@var.info$MQ)
+#quantile(chr1@var.info$DP)
 
 # example plot
 plot(chr1)
+chromoqc(chr1, xlim=c(1e1, 1.1e8))
 
 # example with exporting to pdf -- note path requirements
 # also note zoom ability via xlim=c() option
@@ -31,25 +42,139 @@ dev.off()
 
 
 # Mask poor quality variants
-chr1_masked <- masker(chr1, 
-                                min_QUAL=50,
-                                min_DP=100,
-                                max_DP=50000,
-                                min_MQ=30)
+chr1_masked <- masker(chr1,
+                      min_QUAL=50,
+                      min_DP=1000,
+                      max_DP=10000,
+                      min_MQ=30)
 
 plot(chr1_masked)
+chromoqc(chr1_masked, xlim=c(1e1, 1.1e8))
 
 # Now process the chromR object with proc.chromR
 # The default window size = 1000bp, can set with win.size
-chr1_masked_processed <- proc.chromR(chr1_masked, win.size=50000)
+chr1_proc <- proc.chromR(chr1_masked, win.size = 1e5)
+plot(chr1_proc)
+chromoqc(chr1_proc, xlim=c(1e1, 1.1e8))
 
-head(chr1_masked_processed) # see an overview of each list element
+#head(chr1_masked) # see an overview of each list element
 
-str(chr1_masked_processed)
 
-plot(chr1_masked_processed) #make plots on processed data
+#How to filter a vcf file for minDP
+dp <- extract.gt(vcf, 
+                 element="DP", 
+                 as.numeric=T, 
+                 convertNA=T)
+quantile(dp)
 
-chromoqc(chr1_masked_processed, xlim=c(2e5, 5e7))
+dp[dp==0] <- NA
+
+quantile(dp, na.rm=T)
+
+#pdf(file="~/courses/Ecological_Genomics_24/population_genomics/figures/chromoPlot_chr1.pdf")
+heatmap.bp(dp[1:1000,], rlabels=F, clabels=F)
+#dev.off()
+
+vcf@gt[,-1][is.na(dp)==TRUE] <- NA
+
+indMiss <- apply(dp, MARGIN = 2, function(x){ sum(is.na(x)) })
+indMiss <- indMiss/nrow(vcf@gt)
+hist(indMiss)
+quantile(indMiss)
+
+siteMiss <- apply(dp, MARGIN = 1, function(x){ sum(is.na(x)) })
+siteMiss <- siteMiss/ncol(vcf@gt)
+hist(siteMiss)
+
+vcf@gt = vcf@gt[,c(TRUE, indMiss < 0.5)]
+
+vcf
+
+#vcf = vcf[siteMiss < 0.25,]
+
+dp2 <- extract.gt(vcf, 
+                 element="DP", 
+                 as.numeric=T, 
+                 convertNA=T)
+dp2[dp2==0] <- NA
+quantile(dp2, na.rm=T)
+
+siteMiss <- apply(dp2, MARGIN = 1, function(x){ sum(is.na(x)) })
+siteMiss <- siteMiss/ncol(vcf@gt)
+hist(siteMiss)
+quantile(siteMiss)
+
+vcf
+
+heatmap.bp(dp2[1:1000,], rlabels=F, clabels=F)
+
+#the third is the count and fourth the frequency.
+vcf_maf <- as.data.frame(maf(vcf, element=2)) 
+# element=2 returns info on the minor allele
+hist(vcf_maf$Frequency, breaks=50)
+
+write.vcf(vcf, "~/test.vcf.gz")
+
+
+# indDP <- colMeans(dp2)
+# hist(dp2)
+# hist(indDP)
+
+
+
+
+
+
+myINFO = c("INDEL","DP","AC","AN","MQ") 
+#AC=count of Alt alleles (for doing --mac)
+#AN=count of total (REF+ALT) alleles in called genotypes (after masking??)
+
+vcf_tdy <- vcfR2tidy(vcf, 
+                     info_fields=myINFO,
+                     dot_is_NA=TRUE)
+
+vcf_tdy$meta
+vcf_tdy$fix
+vcf_tdy$gt
+
+
+print(vcf_tdy$gt, n=50)  # view `n` rows in the tibble
+
+# Can extract fields from the `gt` using either vcfR or chom
+indDP = extract.gt(chr1, 
+                   element="DP", 
+                   mask=TRUE, 
+                   as.numeric=TRUE, 
+                   convertNA=TRUE) 
+
+quantile(indDP) # get quantiles
+hist(indDP, xlim=c(0,20),breaks=100) # plot histogram
+mean(indDP) # mean 20X
+
+
+# The first column is the total number of alleles, 
+# the second is the number of NA genotypes, 
+#the third is the count and fourth the frequency.
+vcf_maf <- as.data.frame(maf(vcf, element=2)) 
+# element=2 returns info on the minor allele
+hist(vcf_maf$Frequency, breaks=50)
+
+vcf_tdy_gt = extract_gt_tidy(vcf,
+                             format_fields = c("DP","GT", "PL"),
+                             format_types = TRUE,
+                             dot_is_NA = TRUE,
+                             verbose = TRUE)
+
+
+PL = tidyr::separate_wider_delim(vcf_tdy$gt, cols="gt_PL", ",", names=c("PL1","PL2","PL3"))
+
+pl <- extract.gt(vcf, element = "PL")
+pl_1 <- masplit(pl, sort=1, decreasing=0)
+pl_2 <- masplit(pl, record = 2)
+pl_3 <- masplit(pl, record = 3)
+
+
+#######################################
 
 # Playing with the annotation file
 SEARCHTERM="FRI"
@@ -69,7 +194,7 @@ summary(chr1_masked_processed@win.info$variants)
 ggplot(as.data.frame(chr1_masked_processed@win.info), aes(x=start, y=variants)) +
   geom_point(size=2, shape=2, color="red")
 
-chr1_masked_processed@win.info$genic # what is this?
+#chr1_masked_processed@win.info$genic # what is this?
 
 
 # We can write out the VCF with masking
@@ -79,49 +204,7 @@ write.vcf(chr1_masked_processed,
 
 vcf_field_names(vcf) # gets the INFO names and def's so you can choose what you want
 
-myINFO = c("INDEL","DP","AC","AN","MQ") 
-#AC=count of Alt alleles (for doing --mac)
-#AN=count of total (REF+ALT) alleles in called genotypes (after masking??)
 
-vcf_tdy <- vcfR2tidy(vcf, 
-                     info_fields=myINFO,
-                     dot_is_NA=TRUE)
-
-vcf_tdy$meta
-vcf_tdy$fix
-vcf_tdy$gt
-
-print(vcf_tdy$gt, n=50)  # view `n` rows in the tibble
-
-# Can extract fields from the `gt` tbl using either vcfR or chom
-indDP = extract.gt(chr1_processed_masked, 
-                   element="DP", 
-                   mask=TRUE, 
-                   as.numeric=TRUE, 
-                   convertNA=TRUE) 
-
-quantile(indDP) # get quantiles
-hist(indDP) # plot histogram
-
-# The first column is the total number of alleles, 
-# the second is the number of NA genotypes, 
-#the third is the count and fourth the frequency.
-vcf_maf <- as.data.frame(maf(vcf, element=2)) # element=2 returns info on the minor allele
-hist(vcf_maf$Frequency, breaks=50)
-
-vcf_tdy_gt = extract_gt_tidy(vcf,
-                             format_fields = c("DP","GT", "PL"),
-                             format_types = TRUE,
-                             dot_is_NA = TRUE,
-                             verbose = TRUE)
-
-
-PL = tidyr::separate_wider_delim(vcf_tdy$gt, cols="gt_PL", ",", names=c("PL1","PL2","PL3"))
-
-pl <- extract.gt(vcf, element = "PL")
-pl_1 <- masplit(pl, sort=1, decreasing=0)
-pl_2 <- masplit(pl, record = 2)
-pl_3 <- masplit(pl, record = 3)
 
 ############  NOT WORKING #################
 
