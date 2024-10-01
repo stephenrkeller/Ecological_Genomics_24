b4e80f22e1d681401af490098ba597ea759d1714
stephenrkeller <srkeller@uvm.edu>
2024-10-01 11:55:39 -0400

    update selection script

--- population_genomics/doc/04_selection.r
+++ population_genomics/doc/04_selection.r
@@ -15,16 +15,126 @@ vcf <- read.pcadapt("/gpfs1/cl/pbio3990/PopulationGenomics/variants/vcf_final.fi
 #                    type="lfmm")
 
 
+vcfR <- read.vcfR("~/projects/eco_genomics/population_genomics/outputs/vcf_final.filtered.vcf.gz")
+meta <- read.csv("/gpfs1/cl/pbio3990/PopulationGenomics/metadata/meta4vcf.csv")
+meta2 <- meta[meta$id %in% colnames(vcfR@gt[,-1]),] 
+
+
+# do for component-wise PCAdapt run:
+# component-wise runs for each pc separately
+pcadapt.pca = pcadapt(vcf, 
+                       K=2, 
+                       method="componentwise",
+                       min.maf=0.01,
+                       LD.clumping = list(size=500, thr=0.2))
+summary(pcadapt.pca)
+str(pcadapt.pca)
+
+plot(pcadapt.pca, option="scores", 
+     pop=meta2$region,
+     i=1,j=2)
+
+plot(pcadapt.pca, K=1, option="stat.distribution")
+plot(pcadapt.pca, K=2, option="stat.distribution")
+
+plot(pcadapt.pca, K=1, option="qqplot")
+plot(pcadapt.pca, K=2, option="qqplot")
+
+#plot(pcadapt.pca, option="manhattan")
+
+hist(pcadapt.pca$pvalues, breaks=50, col="gold", 
+     xlab="p-values")
+
+#plot(pcadapt.pca$loadings[,2]) # for PC1 SNP loadings
+
+
+# Bring in VCF file to get info on Chrom/POS
+View(head(vcfR@fix))
+
+vcfR.fix <- as.data.frame(vcfR@fix[,1:2])
+chr.main <- unique(vcfR.fix$CHROM)[1:8]
+
+# We then use the 'seq' function to number the chromosomes from 1 to 8
+chrnum <- as.data.frame(cbind(chr.main, seq(1, 8, 1)))
+
+# And finally merge these numerically named chromosomes with the full diversity results:
+Pval <- pcadapt.pca$pvalues
+Qval <- p.adjust(pcadapt.pca$pvalues, method="BH")
+
+
+pcadapt.MHplot <- cbind(vcfR.fix,Pval)
+
+pcadapt.MHplot <- left_join(chrnum, pcadapt.MHplot, 
+                             join_by(chr.main==CHROM))
+
+
+# create a new "SNP" column that concatenates the chromosome ID with the bp position
+pcadapt.MHplot <- pcadapt.MHplot %>%
+  mutate(SNP=paste0(chr.main,"_",POS))
+
+# Make these 2 fields numbers instead of characters:
+pcadapt.MHplot$V2 = as.numeric(pcadapt.MHplot$V2)
+pcadapt.MHplot$POS = as.numeric(pcadapt.MHplot$POS)
+pcadapt.MHplot$pPC1 = as.numeric(pcadapt.MHplot[,4])
+pcadapt.MHplot$pPC2 = as.numeric(pcadapt.MHplot[,5])
+
+# drop NAs
+pcadapt.MHplot <- pcadapt.MHplot %>% drop_na(pPC1)
+
+# and finally....plot!!!
+pdf("figures/pcadapt_PC1_2_Manhattan.pdf", height=10,width=10)
+par(mfrow=c(2,1))
+manhattan(pcadapt.MHplot,
+          chr="V2",
+          bp="POS",
+          p="pPC1",
+          col=c("blue4","orange3"),
+          cex=0.7,
+          logp=T,
+          ylab="-log10 p-value",
+          genomewideline = F,
+#          suggestiveline = quantPC1,
+          main="PCAdapt genome scan for selection (PC1)")
+
+manhattan(pcadapt.MHplot,
+          chr="V2",
+          bp="POS",
+          p="pPC2",
+          col=c("blue4","orange3"),
+          cex=0.7,
+          logp=T,
+          ylab="-log10 p-value",
+          genomewideline = F,
+#          suggestiveline = quantPC2,
+          main="PCAdapt genome scan for selection (PC2)")
+
+dev.off()
+
+# get outliers after FDR correction
+pcadapt.MHplot$padjPC1 = p.adjust(pcadapt.MHplot$pPC1, method="BH")
+pcadapt.MHplot$padjPC2 = p.adjust(pcadapt.MHplot$pPC2, method="BH")
+
+quantPC1 = -log10(quantile(pcadapt.MHplot$padjPC1, 0.001))
+quantPC2 = -log10(quantile(pcadapt.MHplot$padjPC1, 0.001))
+
+View(pcadapt.MHplot %>%
+       filter(pPC1<0.0001, V2==1) %>%
+       select(chr.main, POS, padjPC1))
+
+View(pcadapt.MHplot %>%
+  filter(pPC2<0.0001, V2==4) %>%
+  select(chr.main, POS, padjPC2))
+
+
+#poplist.names = meta2$region
+
+
+############  below is non-componentwise pcadapt
 pcadapt.pca = pcadapt(vcf,
                       K=2,
                       min.maf=0.01,
                       LD.clumping = list(size=500, thr=0.2))
 
-vcfR <- read.vcfR("~/projects/eco_genomics/population_genomics/outputs/vcf_final.filtered.vcf.gz")
-meta <- read.csv("/gpfs1/cl/pbio3990/PopulationGenomics/metadata/meta4vcf.csv")
-meta2 <- meta[meta$id %in% colnames(vcfR@gt[,-1]),] 
-
-#poplist.names = meta2$region
 
 plot(pcadapt.pca, option="scores", 
      pop=meta2$region,
@@ -91,6 +201,9 @@ pcadapt.MHplot <- pcadapt.MHplot %>% drop_na(Pval)
 
 outliers=pcadapt.MHplot$SNP[which(pcadapt.MHplot$Qval<0.005)]
 
+# which PCs are the outliers associated with?
+outliers.pc <- get.pc(pcadapt.pca,outliers)
+table(outliers.pc$PC)
 
 # and finally....plot!!!
 pdf("figures/pcadapt_K2_Manhattan.pdf", height=5,width=10)
@@ -107,63 +220,6 @@ manhattan(pcadapt.MHplot,
           main="PCAdapt genome scan for selection (K=2)")
 dev.off()
 
-# do for component-wise PCAdapt run:
-# component-wise runs for each pc separately
-pcadapt.pca2 = pcadapt(vcf, 
-                       K=2, 
-                       method="componentwise",
-                       min.maf=0.01,
-                       LD.clumping = list(size=500, thr=0.2))
-summary(pcadapt.pca2)
-str(pcadapt.pca2)
-
-Pval2 <- pcadapt.pca2$pvalues
-
-pcadapt.MHplot2 <- cbind(vcfR.fix,Pval2)
-
-pcadapt.MHplot2 <- left_join(chrnum, pcadapt.MHplot2, 
-                            join_by(chr.main==CHROM))
-
-
-# create a new "SNP" column that concatenates the chromosome ID with the bp position
-pcadapt.MHplot2 <- pcadapt.MHplot2 %>%
-  mutate(SNP=paste0(chr.main,"_",POS))
-
-# Make these 2 fields numbers instead of characters:
-pcadapt.MHplot2$V2 = as.numeric(pcadapt.MHplot2$V2)
-pcadapt.MHplot2$POS = as.numeric(pcadapt.MHplot2$POS)
-pcadapt.MHplot2$pPC1 = as.numeric(pcadapt.MHplot2[,4])
-pcadapt.MHplot2$pPC2 = as.numeric(pcadapt.MHplot2[,5])
-
-# drop NAs
-pcadapt.MHplot2 <- pcadapt.MHplot2 %>% drop_na(pPC1)
-
-# and finally....plot!!!
-pdf("figures/pcadapt_PC1_2_Manhattan.pdf", height=10,width=10)
-par(mfrow=c(2,1))
-manhattan(pcadapt.MHplot2,
-          chr="V2",
-          bp="POS",
-          p="pPC1",
-          col=c("blue4","orange3"),
-          logp=T,
-          ylab="-log10 p-value",
-          genomewideline = F,
-          suggestiveline = 13,
-          main="PCAdapt genome scan for selection (PC1)")
-
-manhattan(pcadapt.MHplot2,
-          chr="V2",
-          bp="POS",
-          p="pPC2",
-          col=c("blue4","orange3"),
-          logp=T,
-          ylab="-log10 p-value",
-          genomewideline = F,
-          suggestiveline = 13,
-          main="PCAdapt genome scan for selection (PC2)")
-
-dev.off()
 
 # SNP2GO annotation and enrichment testing
 library(SNP2GO)
